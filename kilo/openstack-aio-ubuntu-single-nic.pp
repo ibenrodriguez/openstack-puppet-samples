379f828aae0047dab70bc7676dcf68ec18b60e34
Iben Rodriguez <github@ibenit.com>
2015-08-10 22:33:45 -0700

    updated for brix box local install

--- kilo/openstack-aio-ubuntu-single-nic.pp
+++ kilo/openstack-aio-ubuntu-single-nic.pp
@@ -1,18 +1,18 @@
-$admin_password = 'Passw0rd'
+$admin_password = 'pass'
 $demo_password = $admin_password
-$admin_token = '4b46b807-ab35-4a67-9f5f-34bbff2dd439'
-$metadata_proxy_shared_secret = '39c24deb-0d57-4184-81da-fc8ede37082e'
-$region_name = 'RegionOne'
+$admin_token = '4b46b807-ab35-4a67-9f5f-34bbff2dd438'
+$metadata_proxy_shared_secret = '39c24deb-0d57-4184-81da-fc8ede37082f'
+$region_name = 'G14Classified'
 
 $cinder_lvm_loopback_device_size_mb = 10 * 1024
 
 $interface = 'eth0'
-$ext_bridge_interface = 'br-ex'
+$ext_bridge_interface = 'br-ext'
 $dns_nameservers = ['8.8.8.8', '8.8.4.4']
 $private_subnet_cidr = '10.0.0.0/24'
-$public_subnet_cidr = '192.168.209.0/24'
-$public_subnet_gateway = '192.168.209.2'
-$public_subnet_allocation_pools = ['start=192.168.209.30,end=192.168.209.50']
+$public_subnet_cidr = '192.168.247.0/24'
+$public_subnet_gateway = '192.168.247.26'
+$public_subnet_allocation_pools = ['start=192.168.247.130,end=192.168.247.150']
 
 # Note: this is executed on the master
 $gateway = generate('/bin/sh',
